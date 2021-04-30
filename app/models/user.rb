d7b6b8e94deab618460f8df47ddc4a0c022bb7df
Giovanni Alberto <delirable@gmail.com>
2021-04-30 15:30:36 -0500

    [FIR-5493] Add Devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :email, :skills, presence: true
   validates :email, uniqueness: true
 end
--- db/migrate/20210430192139_create_users.rb
+++ db/migrate/20210430192139_create_users.rb
@@ -3,12 +3,10 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :skills, array: true, default: []
       t.string :role
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
